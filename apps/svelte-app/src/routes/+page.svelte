725b38837eb900f7b0aa58be98e9dfeef53bfd6a
Mitya M <m1tyya@proton.me>
2023-04-20 18:59:26 +0200

    Remove svelte template.

--- apps/svelte-app/src/routes/+page.svelte
+++
@@ -1,4 +0,0 @@
-<script lang="ts"></script>
-
-<h1 class="text-gray-700 font-hest text-2xl">Welcome to SvelteKittt</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
